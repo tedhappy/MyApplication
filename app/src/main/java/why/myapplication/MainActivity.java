0f8c28b97f42952c628b9b95f4b72462196736ce
tedhappy <wanghaoyuhappy@163.com>
2017-01-03 23:48:54 +0800

    Initial commit

---
+++ app/src/main/java/why/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package why.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*--------------- jfkdjs ---------------*/
+
+        /*===============  ===============*/
+
+        init();
+        initView();
+        initData();
+        initListener();
+
+        /*###############  ###############*/
+
+
+
+    }
+
+    /**
+     * 初始化
+     */
+    public void init() {
+
+
+    }
+
+    /**
+     * 初始化view
+     */
+    public void initView() {
+
+    }
+
+    /**
+     * 初始化数据
+     */
+    public void initData() {
+
+    }
+
+    /**
+     * 初始化监听
+     */
+    public void initListener() {
+
+    }
+
+
+
+}
